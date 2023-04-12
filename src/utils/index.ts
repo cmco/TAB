5e73bcbc40158f870498701bf5ce590b6b558dea
cedmco <cedrickc@openpay.com.au>
2023-04-12 20:57:27 +0800

    Add utils file for mock data

---
+++ src/utils/index.ts
@@ -0,0 +1,17 @@
+export const careersData = [
+  {
+    title: 'Customer Service Representative ',
+    location: 'Auckland, New zealand',
+    date: '4 days ago',
+  },
+  {
+    title: 'Customer Service Representative ',
+    location: 'Auckland, New zealand',
+    date: '4 days ago',
+  },
+  {
+    title: 'Customer Service Representative ',
+    location: 'Auckland, New zealand',
+    date: '4 days ago',
+  },
+];
